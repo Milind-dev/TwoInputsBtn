8a26f85d828a22a4eaa6cab8e0fa021f726a592b
Milind-dev <milinddev1011@gmail.com>
2022-03-07 15:00:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fp3clp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fp3clp)
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const [firstName, setFirstName] = useState('');
+  const [secondName, setSecondName] = useState('');
+  const [Results, setResults] = useState([]);
+
+  const handleChange = () => {
+    setResults([...firstName, ...secondName]);
+    console.log(firstName, secondName);
+  };
+  return (
+    <div>
+      <h1>Two inputs files</h1>
+      <input
+        type="text"
+        name="firstname"
+        onChange={(e) => setFirstName(e.target.value)}
+        placeholder="firstname"
+      />
+      <input
+        type="text"
+        name="secondname"
+        onChange={(e) => setSecondName(e.target.value)}
+        placeholder="secondname"
+      />
+      <button onClick={handleChange}> click </button>
+      <p>{Results}</p>
+    </div>
+  );
+}
